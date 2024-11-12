f9aee6cbfce19edd8f3ef6f783e0b42f3214e12b
Vanessa Isabel Roque <vroque19@csu.fullerton.edu>
2024-11-11 20:56:42 -0800

    Initial Commit

---
+++ RecipeKeeper/RecipeKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeKeeperApp.swift
+//  RecipeKeeper
+//
+//  Created by Vanessa Roque on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
